f15a2fc8dcdca0989aec222d13d0b875142e78fc
md Miraz Hosen <mdrokonislam773@gmail.com>
2026-04-06 00:37:08 +0600

    sw.js

---
+++ sw.js
@@ -0,0 +1,23 @@
+const cacheName = 'miraz-pro-v2';
+const assets = [
+  './index.html',
+  './manifest.json',
+  'https://cdn.tailwindcss.com',
+  'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(cacheName).then(cache => {
+      cache.addAll(assets);
+    })
+  );
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(res => {
+      return res || fetch(e.request);
+    })
+  );
+});
